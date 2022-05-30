86754310e8a96642c0e6360930575d0ff7378968
ALUNO - CT SISTEMA <ALUNO - CT SISTEMA@DESKTOP-UQO0ATO>
2022-05-30 08:20:49 -0300

    Adicionar arquivos de projeto.

---
+++ MARATONA/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Altura
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto  -  Alturas");
+        Console.WriteLine();
+
+        int N, cont;
+        double soma, media, porcentagem;
+
+        Console.WriteLine("Quantas pessoas serão digitadas?");
+        N = int.Parse(Console.ReadLine());
+
+        string[] nomes = new string[N];
+        int[] idades =  new int[N];
+        float[] alturas = new float[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine("Dados da " + (i + 1) + "ª pessoa");
+            Console.WriteLine("Nome: ");
+            nomes[i] = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            idades[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Altura: ");
+            alturas[i] = float.Parse(Console.ReadLine());
+        }
+
+        soma = 0 ;
+
+        for (int i = 0; i < N; i++)
+        {
+            soma += alturas[i];
+        }
+
+        media = 0 ;
+        media = soma / N;
+
+        Console.WriteLine();
+        Console.WriteLine("Altura média: " + media.ToString("F"));
+
+        cont = 0;
+
+        for(int i = 0; i < N; i++)
+        {
+            if (idades[i] < 16)
+            {
+                cont += 1;
+            }
+        }
+
+        porcentagem = (cont * 100) / N;
+        Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem + "%");
+        
+        for (int i = 0; i < N; i++)
+        {
+            if(idades[i] <= 16)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para encerrar.");
+        Console.ReadLine();
+    }
+}
---
+++ media_pares/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class media_pares
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Projeto  - média pares ");
+        Console.WriteLine();
+
+        int N, i, soma, contPares;
+        double media;
+
+        Console.WriteLine("Quantos elementos vai ter o vetor? ");
+        N = int.Parse(Console.ReadLine());
+
+        int[] vetor = new int[N];
+
+        for (i = 0; i < N; i++)
+        {
+            Console.WriteLine("Digite um número : ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        soma = 0;
+        contPares = 0;
+        
+        for (i = 0; i < N; i++)
+        {
+            if(vetor[i] % 2 == 0)
+            {
+                soma += vetor[i];
+                contPares += 1;
+            }
+        }
+
+        if (contPares == 0)
+        {
+            Console.WriteLine("NENHUM NUMERO PAR");
+        } else
+        {
+            media = soma / contPares;
+            Console.WriteLine("MEDIA DOS PARES = " + media);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para encerrar.");
+        Console.ReadLine();
+    }
+}
